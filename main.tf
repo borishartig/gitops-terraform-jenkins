a5a7cc5d1ea03e0690cea0b8e30906d4064ee434
borishartig <boris.hartig@gmail.com>
2020-06-09 08:19:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=2.0.0"
-  use_msi = true
+  use_msi = "true"
   subscription_id = "25d0e3e3-75da-4395-a34a-9250d6cf1b28"
   tenant_id = "b41b5fb4-7480-4d7d-9735-07cd73bfd2f3"
   features {}
