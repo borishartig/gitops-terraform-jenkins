4eeaa15062f4278af61579742fb07d6a29505ee0
x <x@xamurai.local>
2020-06-27 21:22:44 +0200

    Update Boris

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Configure the Azure Provider
+# Configure the Azure Provider Boris
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=2.0.0"
